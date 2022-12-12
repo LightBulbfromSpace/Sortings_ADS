92a6afb450508fa74b4caf922fa3ace1b46ee5af
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-12-12 23:36:00 +0100

    lab #11

--- lab11/quick_sort.php
+++ lab11/quick_sort.php
@@ -1,1 +1,35 @@
+<?php
 
+function quickSort(array &$arr, int $firstIndex, int $lastIndex) :void
+{
+	if ($firstIndex < $lastIndex)
+	{
+		$p = partition($arr, $firstIndex, $lastIndex);
+		quickSort($arr, $firstIndex, $p);
+		quickSort($arr, $p+1, $lastIndex);
+	}
+}
+
+function partition(array &$arr, int $firstIndex, int $lastIndex) :int
+{
+	$pivot = $arr[($firstIndex + $lastIndex)/2];
+	$i = $firstIndex;
+	$j = $lastIndex;
+	while ($i < $j)
+	{
+		while ($arr[$i] < $pivot)
+		{
+			$i++;
+		}
+		while ($arr[$j] > $pivot)
+		{
+			$j--;
+		}
+		if ($i >= $j)
+		{
+			break;
+		}
+		swap($arr[$i++], $arr[$j--]);
+	}
+	return $j;
+}
