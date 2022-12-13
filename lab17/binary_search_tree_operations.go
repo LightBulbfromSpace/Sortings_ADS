fb9ce6a24fc022a81c33ead04aff8bdec85a38a8
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-12-13 23:09:15 +0100

    binary_search_tree_operations.go: added explanations

--- lab17/binary_search_tree_operations.go
+++ lab17/binary_search_tree_operations.go
@@ -5,6 +5,9 @@ import (
 )
 
 func Search(node *structures.TreeNode, elem int) (*structures.TreeNode, bool) {
+	// два базовых случая: рекурсия пришла к узлу,
+	// который является указателем на ноль => нужного значения в дереве нет;
+	// значение узла соответствует искомому значению => элемент найден.
 	if node.Val == nil {
 		return nil, false
 	}
@@ -12,24 +15,32 @@ func Search(node *structures.TreeNode, elem int) (*structures.TreeNode, bool) {
 		return node, true
 	}
 
+	// используется свойство БДП: левее находятся узлы с элементами меньше данного,
+	// остальные - правее.
 	if elem <= *node.Val {
-		if node.Left == nil {
-			node.Left = new(structures.TreeNode)
+		if node.Left != nil {
+			return Search(node.Left, elem)
 		}
-		return Search(node.Left, elem)
 	} else {
-		if node.Right == nil {
-			node.Right = new(structures.TreeNode)
+		if node.Right != nil {
+			return Search(node.Right, elem)
 		}
-		return Search(node.Right, elem)
 	}
+	return nil, false
 }
 
 func Add(node *structures.TreeNode, elem int) {
+	// базовый случай: значение в узле является указателем на ноль.
+	// проверяется именно значение, а не сам узел, так как узел был
+	// проинициализированн перед вызовом рекурсии, если не существовал.
+	// инициалиализация нового узла перед вызовом рекурсии происходит для
+	// связывания нового узла с остальным девевом.
 	if node.Val == nil {
 		node.Val = &elem
 		return
 	}
+	// используется свойство БДП: левее находятся узлы с элементами меньше данного,
+	// остальные - правее.
 	if elem <= *node.Val {
 		if node.Left == nil {
 			node.Left = new(structures.TreeNode)
@@ -43,6 +54,52 @@ func Add(node *structures.TreeNode, elem int) {
 	}
 }
 
-func Delete(node *structures.TreeNode, elem int) {
-
+func Delete(root *structures.TreeNode, elem int) {
+	node, found := Search(root, elem)
+	// если элемента нет в дереве, его удаление не нужно
+	if !found {
+		return
+	}
+	// удаление листа
+	// листы не удаляются до конца,
+	// так как дерево в данной реализации
+	// не содержит указатель на родителя.
+	if node.Right == nil && node.Left == nil {
+		node.Val = nil
+		return
+	}
+	// удаление узла с одним потомком
+	if node.Right.Val == nil {
+		node.Val = node.Left.Val
+		// адреса сохраняются в переменные, так как иначе они будут потеряны при переносе
+		newRight := node.Left.Right
+		newLeft := node.Left.Left
+		node.Right = newRight
+		node.Left = newLeft
+		return
+	}
+	if node.Left.Val == nil {
+		node.Val = node.Right.Val
+		// адреса сохраняются в переменные, так как иначе они будут потеряны при переносе
+		newRight := node.Right.Right
+		newLeft := node.Right.Left
+		node.Right = newRight
+		node.Left = newLeft
+		return
+	}
+	// удаление узла с двумя потомками
+	// идем на один узел вправо и ищем самый левый
+	newNode := node.Right
+	for newNode.Left != nil {
+		newNode = newNode.Left
+	}
+	// переносим значение найденного узла в удаляемый узел
+	node.Val = newNode.Val
+	// удаляем значение в найденном узле
+	newNode.Val = nil
+	// если у найденного узла есть правый потомок, делаем его перенос
+	if newNode.Right != nil {
+		newNode.Val = node.Right.Val
+		newNode.Right = node.Right.Right
+	}
 }
--- lab17/binary_search_tree_operations_test.go
+++ lab17/binary_search_tree_operations_test.go
@@ -16,7 +16,7 @@ func TestSearch(t *testing.T) {
 		elem    int
 		found   bool
 	}{
-		//{"3(, 4)", 5, false},
+		{"3(, 4)", 5, false},
 		{"3(4, 5(6, 7))", 5, true},
 		{"4(, 5)", 4, true},
 		{"8(3(1, 6(4, 7)), 10(, 14(13, )))", 9, false},
@@ -56,6 +56,31 @@ func TestAdd(t *testing.T) {
 			assert.NoError(t, err)
 			labtest.FailAfter(t, 500*time.Millisecond, func() {
 				Add(got, tc.elem)
+			})
+			assert.Equal(t, tc.expected, tree_lab.OutputTreeCLI(got))
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	cases := []struct {
+		initial  string
+		elem     int
+		expected string
+	}{
+		{"3(, 4(, 5))", 5, "3(, 4(, ))"},
+		{"3(, 5(4, ))", 5, "3(, 4)"},
+		{"3(, 5(, 6))", 5, "3(, 6)"},
+		{"4(3(1, 2), 5)", 3, "4(2(1, ), 5)"},
+		{"8(4(1, 6(5, 7)), 8)", 4, "8(5(1, 6(, 7)), 8)"},
+		{"8(3(1, 6(4, 7)), 10(9, 14(13, )))", 9, "8(3(1, 6(4, 7)), 10(, 14(13, )))"},
+	}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("test #%d for %v", i+1, tc.expected), func(t *testing.T) {
+			got, err := tree_lab.InputTreeCLI(tc.initial)
+			assert.NoError(t, err)
+			labtest.FailAfter(t, 500*time.Millisecond, func() {
+				Delete(got, tc.elem)
 			})
 			assert.Equal(t, tc.expected, tree_lab.OutputTreeCLI(got))
 		})
