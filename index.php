473893b3e6b4d89b0eaf30146b9fc3626cb1a4d9
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-12-14 10:26:38 +0100

    index.php: added cases

--- index.php
+++ index.php
@@ -4,13 +4,18 @@ require_once 'boot.php';
 
 function main()
 {
-	$arr = [2, 1, 3, 9, 4, 5, 33, 12, 6, 55, 77, 88, 66, 100, 99];
-	//heapSort($arr, count($arr));
-	//mergeSort($arr, 0, count($arr));
-	quickSort($arr, 0, count($arr) - 1);
-	echo'<pre>';
-	var_dump($arr);
-	echo'</pre>';
+	$arrs = [];
+	$arrs[] = [0, 1, 3, 9, 4, 5, 33, 12, 6, 55, 77, 88, 66, 100, 99];
+	$arrs[] = [200, 33, 12, 6, 55, 77, 88, 66, 100, 99, 10, 9, 2, 1];
+	foreach ($arrs as $arr)
+	{
+		heapSort($arr, count($arr));
+		//mergeSort($arr, 0, count($arr));
+		//quickSort($arr, 0, count($arr) - 1);
+		echo'<pre>';
+		var_dump($arr);
+		echo'</pre>';
+	}
 }
 
 main();
