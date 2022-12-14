09182f106847acae344ee8b5b12192ac9c4b0c8a
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-12-14 02:14:06 +0100

    heap_sort.php: added explanation

--- lab_9/heap_sort.php
+++ lab_9/heap_sort.php
@@ -1,20 +1,32 @@
 <?php
 
+require_once ROOT . '/boot.php';
+
+// вспомогательная функция для heapsort,
+// сортирует маленькие кучки из трех элементов
+// и рекурсивно кучку справа, если наибольший
+// элемент был в правом узле и слева, если
+// соответствующий элемент был слева
 function heapify(int $currentIndex, array &$arr, int $arrSize)
 {
-	$index = 2 * $currentIndex + 1;
+	// индекс левого узла при представлении кучи в виде бинарного дерева
+	$indexLeft = 2 * $currentIndex + 1;
+	// с помощью следующих двух ветвей if отыскивается индекс наибольшего
+	// элемента среди данного элемента, узла справа и слева.
 	$theBiggestNumIndex = $currentIndex;
-	if ($index < $arrSize && $arr[$theBiggestNumIndex] < $arr[$index])
+	if ($indexLeft < $arrSize && $arr[$theBiggestNumIndex] < $arr[$indexLeft])
 	{
-		$theBiggestNumIndex = $index;
+		$theBiggestNumIndex = $indexLeft;
 	}
 
-	$index++;
-	if ($index < $arrSize && $arr[$theBiggestNumIndex] < $arr[$index])
+	// индекс правого узла при представлении кучи в виде бинарного дерева
+	$indexRight = $indexLeft + 1;
+	if ($indexRight < $arrSize && $arr[$theBiggestNumIndex] < $arr[$indexRight])
 	{
-		$theBiggestNumIndex = $index;
+		$theBiggestNumIndex = $indexRight;
 	}
 
+	// если наибольший элемент не сверху, то он поднимается наверх
 	if ($theBiggestNumIndex !== $currentIndex)
 	{
 		swap($arr[$theBiggestNumIndex],$arr[$currentIndex]);
@@ -22,28 +34,25 @@ function heapify(int $currentIndex, array &$arr, int $arrSize)
 	}
 }
 
-
+// Пирамидальная сортировка
 function heapSort(&$arr, $arrSize)
 {
+	// функция построения кучек вызывается для всех элементов
+	// снизу вверх, кроме листьев (при представлении кучи бинарным деревом),
+	// так как листья уже находятся в правильном месте относительно
+	// null-потомков. Строится куча.
 	for ($i = $arrSize / 2 - 1; $i >= 0; $i--)
 	{
 		heapify($i, $arr, $arrSize);
 	}
-
+	// Наверху кучи - самый большой элемент.
+	// Данный цикл занимается тем, что меняет местами корень
+	// и элемент с последним индексом и восстанавливает кучу
+	// уже без элемента с последним индексом. Таким в конце начинает
+	// образовываться отсортированный массив.
 	for ($i = $arrSize - 1; $i >= 0; $i--)
 	{
 		swap($arr[$i], $arr[0]);
 		heapify(0, $arr, $i);
 	}
-}
-
-function swap(&$a, &$b): void
-{
-	if ($a === $b) {
-		return;
-	}
-
-	$tmp = $a;
-	$a = $b;
-	$b = $tmp;
 }
