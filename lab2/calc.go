c8f5eb95c21f31081fe502b9af6b115b969a5629
Pavel Volkov <potschta.00@mail.ru>
2022-12-19 20:23:49 +0100

    calc.go: completed function calculating postfix notation

--- lab2/calc.go
+++ lab2/calc.go
@@ -3,6 +3,7 @@ package calc_math_exp
 import (
 	"errors"
 	"labs/structures"
+	"math"
 	"strconv"
 	"strings"
 )
@@ -17,13 +18,16 @@ var opPriorities = map[string]int{
 	"~": 4,
 }
 
-func ToPolishNotation(exp string) ([]string, error) {
+func toPolishNotation(exp string) ([]string, error) {
 	var result []string
 	var operators structures.Stack[string]
-
 	var numStr strings.Builder
 
 	for i, ch := range strings.Split(exp, "") {
+		// пропуск пробела
+		if ch == " " {
+			continue
+		}
 		// создается строка числа
 		_, err := strconv.Atoi(ch)
 		if err == nil || (ch == "." && numStr.Len() > 0) {
@@ -85,4 +89,61 @@ func ToPolishNotation(exp string) ([]string, error) {
 		result = append(result, elem)
 	}
 	return result, nil
+}
+
+func CalcPostfixNotation(exp string) (result float64, err error) {
+	var nums structures.Stack[float64]
+	postfixNotation, err := toPolishNotation(exp)
+	if err != nil {
+		return 0.0, err
+	}
+	for _, item := range postfixNotation {
+		// если елемент является числом, заносим его в стек
+		num, err := strconv.ParseFloat(item, 64)
+		if err == nil {
+			nums.Push(num)
+			continue
+		}
+		if _, ok := opPriorities[item]; ok {
+			if item == "~" {
+				num, err = nums.Pop()
+				if err != nil {
+					return 0, err
+				}
+				num = -num
+			} else {
+				secondNum, e := nums.Pop()
+				if e != nil {
+					return 0, e
+				}
+				firstNum, e := nums.Pop()
+				if e != nil {
+					return 0, e
+				}
+				num, e = executeOperation(firstNum, secondNum, item)
+				if e != nil {
+					return 0, e
+				}
+			}
+			nums.Push(num)
+		}
+	}
+	return nums.Pop()
+}
+
+func executeOperation(n1, n2 float64, op string) (float64, error) {
+	switch op {
+	case "+":
+		return n1 + n2, nil
+	case "-":
+		return n1 - n2, nil
+	case "*":
+		return n1 * n2, nil
+	case "/":
+		return n1 / n2, nil
+	case "^":
+		return math.Pow(n1, n2), nil
+	default:
+		return 0, errors.New("operator not found")
+	}
 }
--- lab2/calc_test.go
+++ lab2/calc_test.go
@@ -3,26 +3,40 @@ package calc_math_exp
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	test "labs/testing"
 	"strings"
 	"testing"
 )
 
+var cases = []struct {
+	exp, postfixNotation string
+	result               float64
+}{
+	{"2+3=", "2 3 +", 5},
+	{"33-(22+44)=", "33 22 44 + -", -33},
+	{"(323 / (96 * 4 + 8 * 7))=", "323 96 4 * 8 7 * + /", 0.7340909091},
+	{"3+4*2/(1-5)^2", "3 4 2 * 1 5 - 2 ^ / +", 3.5},
+	{"3/9-5=", "3 9 / 5 -", -4.6666666667},
+	{"-2+7*(3/9)-5=", "2 ~ 7 3 9 / * + 5 -", -4.6666666667},
+	{"5.5 + 6.6", "5.5 6.6 +", 12.1},
+}
+
 func TestToPolishNotation(t *testing.T) {
-	cases := []struct {
-		exp, result string
-	}{
-		{"2+3=", "2 3 +"},
-		{"33-(22+44)=", "33 22 44 + -"},
-		{"(323/(96*4+8*7))=", "323 96 4 * 8 7 * + /"},
-		{"3+4*2/(1-5)^2", "3 4 2 * 1 5 - 2 ^ / +"},
-		{"3/9-5=", "3 9 / 5 -"},
-		{"-2+7*(3/9)-5=", "2 ~ 7 3 9 / * + 5 -"},
-	}
 	for _, tc := range cases {
 		t.Run(fmt.Sprintf("test for %s", tc.exp), func(t *testing.T) {
-			got, err := ToPolishNotation(tc.exp)
+			got, err := toPolishNotation(tc.exp)
 			assert.NoError(t, err)
-			assert.Equal(t, tc.result, strings.Join(got, " "))
+			assert.Equal(t, tc.postfixNotation, strings.Join(got, " "))
+		})
+	}
+}
+
+func TestCalcPostfixNotation(t *testing.T) {
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("test for %s", tc.exp), func(t *testing.T) {
+			got, err := CalcPostfixNotation(tc.exp)
+			assert.NoError(t, err)
+			test.AssertEqualFloat64(t, got, tc.result)
 		})
 	}
 }
