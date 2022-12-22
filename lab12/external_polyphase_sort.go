5c255ae4316238121f7ed0c7b09453182265d487
Pavel Volkov <potschta.00@mail.ru>
2022-12-22 02:11:05 +0100

    external polyphase sort in development

--- lab12/external_polyphase_sort.go
+++ lab12/external_polyphase_sort.go
@@ -1,1 +1,396 @@
-package lab12
+package external_polyphase_sort
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"math"
+	"math/rand"
+	"os"
+	"strconv"
+	"time"
+)
+
+//func ExternalPolyphaseSort(firstFile, secondFile, thirdFile io.ReadWriteSeeker) (io.ReadWriteSeeker, error) {
+//	var err error
+//	// true - end of file belongs to first file,
+//	// false - end of file belongs to second file
+//	var EOFofFirstFile bool
+//	for err != io.EOF {
+//		var seq1 []byte
+//		seq1, err = readSequence(firstFile)
+//		if err != nil {
+//			if err == io.EOF {
+//				EOFofFirstFile = true
+//				continue
+//			}
+//			return nil, err
+//		}
+//
+//		var seq2 []byte
+//		seq2, err = readSequence(secondFile)
+//		if err != nil {
+//			if err == io.EOF {
+//				EOFofFirstFile = false
+//				continue
+//			}
+//			return nil, err
+//		}
+//		seq := mergeSequences(seq1, seq2)
+//		_, err = thirdFile.Write(seq)
+//		if err != nil {
+//			return nil, err
+//		}
+//	}
+//	if EOFofFirstFile {
+//		for err != io.EOF {
+//
+//		}
+//	}
+//}
+
+//func Round(firstFile, secondFile, thirdFile io.ReadWriteSeeker) error {
+//	var nthFileisEmply int
+//	seq1, err := readSequence(firstFile)
+//	if err != nil {
+//		if err == io.EOF {
+//			nthFileisEmply = 1
+//		} else {
+//			return err
+//		}
+//	}
+//
+//	seq2, err := readSequence(secondFile)
+//	if err != nil {
+//		if err == io.EOF {
+//			nthFileisEmply = 2
+//		} else {
+//			return err
+//		}
+//	}
+//
+//	seq := mergeSequences(seq1, seq2)
+//	_, err = thirdFile.Write(seq)
+//	if err != nil {
+//		return err
+//	}
+//	return nil
+//}
+
+func ExternalPolyphaseSort(initialFile *os.File, maxSeqInRAM int) (*os.File, error) {
+	info, err := initialFile.Stat()
+	if err != nil {
+		return nil, err
+	}
+	initialFileSize := info.Size()
+	numberOfTmpFiles := int(math.Ceil(float64(initialFileSize) / float64(maxSeqInRAM)))
+	numberOfTmpFiles += numberOfTmpFiles % 2
+	tmpFileSize := int(math.Ceil(float64(initialFileSize) / float64(numberOfTmpFiles)))
+
+	//fmt.Printf("numberOfTmpFiles = %d, tmpFileSize = %d\n", numberOfTmpFiles, tmpFileSize)
+
+	// divide information from initial file to several files
+	var tmpFiles []*os.File
+	for i := 0; i < numberOfTmpFiles; i++ {
+		//buffer := make([]byte, tmpFileSize)
+		//
+		//n, err := initialFile.Read(buffer)
+		//if err != nil && err != io.EOF {
+		//	return nil, err
+		//}
+		//
+		//buffer = buffer[:n]
+		//fmt.Println(buffer)
+		//nums := byteSlicetoIntSlice(buffer)
+		//fmt.Println(*nums)
+		nums, err := readMaxAmountOfNums(initialFile, tmpFileSize)
+		if err != nil {
+			return nil, err
+		}
+		QuickSort(nums)
+
+		fmt.Printf("nums = %d\n", *nums)
+
+		tmp, err := os.Create(createTmpFileName())
+		if err != nil {
+			return nil, err
+		}
+
+		buffer := intsToBytesSlice(*nums...)
+
+		n, err := tmp.Write(buffer)
+		fmt.Printf("n = %d\n", n)
+		if err != nil {
+			return nil, err
+		}
+
+		tmpFiles = append(tmpFiles, tmp)
+	}
+
+	// merge files into one decreasing number of files in two times with each interation
+	for numberOfTmpFiles > 1 {
+		var tmpFiles2 []*os.File
+		for i := 0; i < numberOfTmpFiles-1; i++ {
+			tmp, err := os.Create(createTmpFileName())
+			if err != nil {
+				return nil, err
+			}
+			tmp, err = mergeSortedSequencesInFiles(tmpFiles[i], tmpFiles[i+1], tmp)
+			if err != nil {
+				return nil, err
+			}
+			tmpFiles2 = append(tmpFiles2, tmp)
+		}
+		//for _, file := range tmpFiles {
+		//	err := os.Remove(file.Name())
+		//	if err != nil {
+		//		return nil, err
+		//	}
+		//}
+		tmpFiles = tmpFiles2
+		numberOfTmpFiles >>= 1
+	}
+	initialFile.Seek(0, 0)
+	//mergeSortedSequencesInFiles(tmpFiles[0], tmpFiles[1], initialFile)
+	return tmpFiles[0], nil
+}
+
+func readMaxAmountOfNums(file *os.File, maxLength int) (*[]int, error) {
+	var (
+		err   error
+		nums  []int
+		n, nc int
+	)
+	for n < maxLength {
+		var num int
+		num, nc, err = getNum(file)
+		if nc > maxLength {
+			return nil, errors.New("not enough memory: numbers in file are too big")
+		}
+		if err != nil {
+			if err == io.EOF {
+				return &nums, err
+			}
+			return nil, err
+		}
+		if n > maxLength {
+			file.Seek(-int64(nc), 1)
+			break
+		}
+		nums = append(nums, num)
+		n += nc
+	}
+
+	return &nums, nil
+}
+
+func createTmpFileName() string {
+	rand.Seed(time.Now().Unix())
+	return fmt.Sprintf("tmp_%d.dat", rand.Int63())
+}
+
+func byteSlicetoIntSlice(bytes []byte) *[]int {
+	var nums []int
+	var num int
+	for _, ch := range bytes {
+		if ch > 47 || ch < 58 {
+			num = num*10 + (int(ch) - 48)
+		} else {
+			nums = append(nums, num)
+			num = 0
+		}
+	}
+	fmt.Printf("nums = %q", nums)
+	return &nums
+}
+
+func mergeSortedSequencesInFiles(inputFile1, inputFile2, outputFile *os.File) (*os.File, error) {
+	for true {
+		n1, read1, err := getNum(inputFile1)
+		if err != nil {
+			if err == io.EOF {
+				inputFile1.Seek(-int64(read1), 1)
+				break
+			}
+			return nil, err
+		}
+		n2, read2, err := getNum(inputFile2)
+		if err != nil {
+			if err == io.EOF {
+				inputFile2.Seek(-int64(read2), 1)
+				break
+			}
+			return nil, err
+		}
+		var num int
+		if n1 < n2 {
+			num = n1
+			inputFile2.Seek(-int64(read2), 1)
+		} else {
+			num = n2
+			inputFile1.Seek(-int64(read1), 1)
+		}
+
+		_, err = outputFile.Write(intsToBytesSlice(num))
+		if err != nil {
+			return nil, err
+		}
+	}
+	var err error
+	for err != io.EOF {
+		var n1 int
+		n1, _, err = getNum(inputFile1)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		_, e := outputFile.Write(intsToBytesSlice(n1))
+		if e != nil {
+			return nil, err
+		}
+	}
+
+	for err != io.EOF {
+		var n2 int
+		n2, _, err = getNum(inputFile2)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		_, e := outputFile.Write(intsToBytesSlice(n2))
+		if e != nil {
+			return nil, err
+		}
+	}
+	return outputFile, nil
+}
+
+func intsToBytesSlice(nums ...int) (result []byte) {
+	for _, n := range nums {
+		result = append(result, []byte(strconv.Itoa(n))...)
+		result = append(result, byte(0x20))
+	}
+	return result
+}
+
+func QuickSort(seq *[]int) {
+	quickSortRecursion(seq, 0, len(*seq)-1)
+}
+
+func quickSortRecursion(seq *[]int, firstIndex, lastIndex int) {
+	if firstIndex < lastIndex {
+		p := partition(seq, firstIndex, lastIndex)
+		quickSortRecursion(seq, firstIndex, p)
+		quickSortRecursion(seq, p+1, lastIndex)
+	}
+}
+
+func partition(seq *[]int, firstIndex, lastIndex int) int {
+	pivot := (*seq)[(firstIndex+lastIndex)/2]
+	i := firstIndex
+	j := lastIndex
+	for i < j {
+		for (*seq)[i] < pivot {
+			i++
+		}
+		for (*seq)[j] > pivot {
+			j--
+		}
+		if i >= j {
+			break
+		}
+		(*seq)[i], (*seq)[j] = (*seq)[j], (*seq)[i]
+		i++
+		j--
+	}
+	return j
+}
+
+func mergeSequences(seq1, seq2 []int) (seq []int) {
+	it1, it2 := 0, 0
+	for it1 < len(seq1) && it2 < len(seq2) {
+		if seq1[it1] < seq2[it2] {
+			seq = append(seq, seq1[it1])
+			it1++
+		} else {
+			seq = append(seq, seq2[it2])
+			it2++
+		}
+	}
+	for ; it1 < len(seq1); it1++ {
+		seq = append(seq, seq1[it1])
+	}
+	for ; it2 < len(seq2); it2++ {
+		seq = append(seq, seq2[it2])
+	}
+
+	return
+}
+
+func readSequence(file *os.File) (seq []int, n int, err error) {
+	for true {
+		num, nc, err := getNum(file)
+		n += nc
+		if err != nil && err != io.EOF {
+			return nil, 0, err
+		}
+		if len(seq) == 0 {
+			seq = append(seq, num)
+			continue
+		}
+		if nc > 0 {
+			if seq[len(seq)-1] > num {
+				nInt64 := int64(nc)
+				nInt64 = -nInt64
+				file.Seek(nInt64, 1)
+				n -= nc
+				break
+			}
+			seq = append(seq, num)
+		}
+		if err == io.EOF {
+			break
+		}
+	}
+
+	return seq, n, nil
+}
+
+func getNum(file *os.File) (num int, n int, err error) {
+	var char byte
+	var numStr []byte
+	for true {
+		char, err = getChar(file)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return 0, 0, err
+		}
+		n++
+		if char < 48 || char > 57 {
+			break
+		}
+		numStr = append(numStr, char)
+	}
+	if n > 0 {
+		var e error
+		num, e = strconv.Atoi(string(numStr))
+		if e != nil {
+			return num, n, e
+		}
+	}
+	return num, n, err
+}
+
+func getChar(file *os.File) (byte, error) {
+	charBuff := make([]byte, 1)
+
+	n, err := file.Read(charBuff)
+	if n == 0 || err != nil {
+		if err == io.EOF {
+			return charBuff[0], err
+		}
+		return 0x00, err
+	}
+
+	return charBuff[0], nil
+}
--- lab12/external_polyphase_sort_test.go
+++ lab12/external_polyphase_sort_test.go
@@ -1,1 +1,288 @@
 package external_polyphase_sort
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"os"
+	"testing"
+)
+
+//func TestExternalPolyphaseSort(t *testing.T) {
+//	cases := []struct{
+//		testFileSystems *fstest.MapFS
+//		result *fstest.MapFile
+//	}{
+//		{
+//			&fstest.MapFS{
+//				"a.txt" : &fstest.MapFile{Data: []byte("4 1 85 23 45 56 78 2 3 2 1 43")},
+//				"b.txt" : &fstest.MapFile{Data: []byte("7 3342 2341 34 1 234 67 3423 4556 76553")},
+//				"c.txt" : &fstest.MapFile{},
+//			},
+//			&fstest.MapFile{Data: []byte("1 1 1 2 2 3 4 7 23 34 45 56 67 85 ...")},
+//		},
+//
+//		{
+//			&fstest.MapFS{
+//			"a.txt" : &fstest.MapFile{Data: []byte("4 1 85  2  43")},
+//			"b.txt" : &fstest.MapFile{Data: []byte("7 3342 2341 34 1")},
+//			"c.txt" : &fstest.MapFile{},
+//			},
+//			&fstest.MapFile{Data: []byte("1 1 2 4 7 34 43 85 2341 3342")},
+//		},
+//	}
+//	for i, tc := range cases {
+//		t.Run(fmt.Sprintf("test #%d", i), func(t *testing.T) {
+//			f1, _ := os.Open("a.txt")
+//			f2, _ := os.Open("b.txt")
+//			f3, _ := os.Open("c.txt")
+//			got, err := ExternalPolyphaseSort(f1, f2, f3)
+//			assert.NoError(t, err)
+//			assert.Equal(t, *tc.result, got)
+//		})
+//	}
+//}
+
+type file struct {
+	name string
+	data string
+}
+type testFilesystem struct {
+	files  []*file
+	result *file
+}
+
+func TestExternalPolyphaseSort(t *testing.T) {
+
+	cases := []struct {
+		initialFile file
+		maxMemory   int
+		expected    string
+	}{
+		//{
+		//	file{"a.dat", "4 1 85 2 43"},
+		//	3,
+		//	"1 2 4 85 1 43",
+		//},
+		{
+			file{"b.dat", "7 64 53454 545 97 124 827 3342 2341 34 1"},
+			12,
+			"1 7 34 64 97 124 545 827 2341 3342 53454",
+		},
+	}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("test #%d", i+1), func(t *testing.T) {
+			tmp, err, rm := createTmpFile(&tc.initialFile)
+
+			defer rm()
+
+			got, err := ExternalPolyphaseSort(tmp, tc.maxMemory)
+			if err != nil && err != io.EOF {
+				t.Errorf("unexpected error: %q", err)
+			}
+
+			//fmt.Println(len(got))
+
+			buffer := make([]byte, len(tc.expected))
+			got.Seek(0, 0)
+			n, _ := got.Read(buffer)
+			fmt.Println(n)
+
+			assert.Equal(t, tc.expected, string(buffer))
+		})
+	}
+}
+
+func TestReadMaxAmountOfNums(t *testing.T) {
+	cases := []struct {
+		initialFile file
+		maxMemory   int
+		expected    *[]int
+	}{
+		{
+			file{"a.dat", "4 1 85 2 43"},
+			4,
+			&[]int{4, 1},
+		},
+		{
+			file{"b.dat", "7 64 53454 545 97 124 827 3342 2341 34 1"},
+			8,
+			&[]int{7, 64, 53454},
+		},
+	}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("test for #%d", i+1), func(t *testing.T) {
+			tmp, err, rm := createTmpFile(&tc.initialFile)
+			assert.NoError(t, err)
+			defer rm()
+
+			nums, err := readMaxAmountOfNums(tmp, tc.maxMemory)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tc.expected, nums)
+		})
+	}
+}
+
+func TestMergeSortedsequencesInFiles(t *testing.T) {
+	cases := []*testFilesystem{
+		{
+			[]*file{
+				&file{"a.dat", "1 2 4 43 85 87"},
+				&file{"b.dat", "1 7 34 2347 3342"},
+				&file{"c.dat", ""},
+			},
+			&file{"c.dat", "1 1 2 4 7 34 43 85 87 2347 3342"},
+		},
+	}
+	for i, fs := range cases {
+		t.Run(fmt.Sprintf("test for #%d", i+1), func(t *testing.T) {
+			tmps, result, delFuncs, err := createAllTmpFiles(fs)
+			assert.NoError(t, err)
+
+			got, err := mergeSortedSequencesInFiles(tmps[0], tmps[1], result)
+			assert.NoError(t, err)
+
+			buffer := make([]byte, len(fs.result.data))
+			got.Seek(0, 0)
+			got.Read(buffer)
+			//assert.NoError(t, err)
+
+			assert.Equal(t, fs.result.data, string(buffer))
+
+			for _, rm := range delFuncs {
+				rm()
+			}
+		})
+	}
+}
+
+func TestQuickSort(t *testing.T) {
+	cases := []struct {
+		initial, expected []int
+	}{
+		{[]int{2, 20, 7, 2341, 3342, 43, 3, 4}, []int{2, 3, 4, 7, 20, 43, 2341, 3342}},
+	}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("test for #%d", i+1), func(t *testing.T) {
+			QuickSort(&tc.initial)
+			assert.Equal(t, tc.expected, tc.initial)
+		})
+	}
+}
+
+func TestMergeSequences(t *testing.T) {
+	cases := []struct {
+		seq1, seq2, expected []int
+	}{
+		{[]int{2, 20, 43}, []int{3, 4, 7, 2341, 3342}, []int{2, 3, 4, 7, 20, 43, 2341, 3342}},
+	}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("test for #%d", i+1), func(t *testing.T) {
+			got := mergeSequences(tc.seq1, tc.seq2)
+			assert.Equal(t, tc.expected, got)
+		})
+	}
+}
+
+func TestReadSequence(t *testing.T) {
+	cases := []struct {
+		fl       *file
+		expected []int
+	}{
+		{&file{"a.dat", "1 4 85 2 43"}, []int{1, 4, 85}},
+		{&file{"b.dat", "3 4 7 2341 3342"}, []int{3, 4, 7, 2341, 3342}},
+		{&file{"c.dat", "4 1 85 2 43"}, []int{4}},
+	}
+	var deleteFuncs []func()
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("test for #%d", i+1), func(t *testing.T) {
+			tmp, err, del := createTmpFile(tc.fl)
+			assert.NoError(t, err)
+			deleteFuncs = append(deleteFuncs, del)
+
+			seq, _, err := readSequence(tmp)
+			//tmp.Seek(int64(n), 1)
+			if err != nil && err != io.EOF {
+				t.Errorf("unexpected error: %q", err)
+			}
+			assert.Equal(t, tc.expected, seq)
+		})
+	}
+
+	t.Run("second read from file", func(t *testing.T) {
+		tmp, err := os.Open("a.dat")
+		assert.NoError(t, err)
+		readSequence(tmp)
+
+		seq, _, err := readSequence(tmp)
+		if err != nil && err != io.EOF {
+			t.Errorf("unexpected error: %q", err)
+		}
+		assert.Equal(t, []int{2, 43}, seq)
+	})
+
+	for _, rm := range deleteFuncs {
+		rm()
+	}
+}
+
+func TestGetNum(t *testing.T) {
+	cases := []struct {
+		input  *file
+		result int
+	}{
+		{&file{"a.txt", "678"}, 678},
+		{&file{"a.txt", "342 234"}, 342},
+	}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("test #%d", i+1), func(t *testing.T) {
+
+			tmp, err, rm := createTmpFile(tc.input)
+			assert.NoError(t, err)
+			defer rm()
+
+			got, _, err := getNum(tmp)
+			if err != nil && err != io.EOF {
+				t.Errorf("unexpected error: %q", err)
+			}
+
+			assert.Equal(t, tc.result, got)
+		})
+	}
+}
+
+func createAllTmpFiles(fs *testFilesystem) (initialFiles []*os.File, resultFile *os.File, deleteFuncs []func(), err error) {
+	for _, fl := range fs.files {
+		tmp, err, rm := createTmpFile(fl)
+		if err != nil {
+			return nil, nil, nil, err
+		}
+		deleteFuncs = append(deleteFuncs, rm)
+		initialFiles = append(initialFiles, tmp)
+	}
+	resultFile, err, rm := createTmpFile(fs.result)
+	if err != nil {
+		return nil, nil, nil, err
+	}
+	deleteFuncs = append(deleteFuncs, rm)
+	return
+}
+
+func createTmpFile(fl *file) (tmp *os.File, err error, delete func()) {
+	tmp, err = os.Create(fl.name)
+	if err != nil {
+		return nil, err, nil
+	}
+	delete = func() {
+		os.Remove(fl.name)
+	}
+
+	_, err = tmp.Write([]byte(fl.data))
+	tmp.Seek(0, 0)
+	if err != nil {
+		return nil, err, nil
+	}
+
+	return tmp, nil, delete
+}
