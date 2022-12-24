d37664bfad5d672fc9ab2e5443c84d02e29f76c5
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-12-24 19:27:43 +0200

    quick_sort.php: added Lomuto's partition

--- lab11/quick_sort.php
+++ lab11/quick_sort.php
@@ -5,11 +5,16 @@
 // Сначала происходит подсчет точки деления $p и сортировка относительно этой точки
 // (подробнее в описании partition).
 // Далее quickSort вызывается для полученных частей массива.
-function quickSort(array &$arr, int $firstIndex, int $lastIndex) :void
+
+// $partitionType = false - Hoar partition, true - Lomuto partition
+function quickSort(array &$arr, int $firstIndex, int $lastIndex, bool $partitionType = false) :void
 {
 	if ($firstIndex < $lastIndex)
 	{
-		$p = partition($arr, $firstIndex, $lastIndex);
+		$p = $partitionType ? partitionLomuto($arr, $firstIndex, $lastIndex)
+			: partitionHoar($arr, $firstIndex, $lastIndex);
+		$p = $partitionType ? $p - 1 : $p;
+
 		quickSort($arr, $firstIndex, $p);
 		quickSort($arr, $p+1, $lastIndex);
 	}
@@ -28,7 +33,7 @@ function quickSort(array &$arr, int $firstIndex, int $lastIndex) :void
 // Как только находится элемент слева больший $pivot и справа меньший $pivot,
 // данные элементы меняются местами. Работа функции заканчивается, как только
 // индексы совпадут или значение индекса, идущего слева, будет больше идущего справа.
-function partition(array &$arr, int $firstIndex, int $lastIndex) :int
+function partitionHoar(array &$arr, int $firstIndex, int $lastIndex) :int
 {
 	$pivot = $arr[($firstIndex + $lastIndex)/2];
 	$i = $firstIndex;
@@ -50,4 +55,18 @@ function partition(array &$arr, int $firstIndex, int $lastIndex) :int
 		swap($arr[$i++], $arr[$j--]);
 	}
 	return $j;
+}
+
+function partitionLomuto(array &$arr, int $firstIndex, int $lastIndex) :int
+{
+	$pivot = $arr[$lastIndex];
+	$i = $firstIndex-1;
+		for ($j = $firstIndex; $j <= $lastIndex; $j++)
+		{
+			if ($arr[$j] <= $pivot)
+			{
+				swap($arr[++$i], $arr[$j]);
+			}
+		}
+		return $i;
 }
