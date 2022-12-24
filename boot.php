521c12dd1957bb3b64d4da9b0a04601ad177d728
PV <101756515+LightBulbfromSpace@users.noreply.github.com>
2022-12-25 01:18:06 +0200

    index.php, boot.php: insignificant changes

--- boot.php
+++ boot.php
@@ -6,3 +6,5 @@ require_once ROOT . '/service.php';
 require_once ROOT . '/lab_9_heap_sort/heap_sort.php';
 require_once ROOT . '/lab_10_merge_sort/merge_sort.php';
 require_once ROOT . '/lab_11_quick_sort/quick_sort.php';
+require_once ROOT . '/counting_sort/counting_sort.php';
+require_once ROOT . '/bucket_sort/bucket_sort.php';
